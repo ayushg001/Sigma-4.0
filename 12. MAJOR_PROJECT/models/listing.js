1fb6ee3fc1b920fe2ec4a9b5367f6ac77c042ab5
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-02-11 19:15:11 +0530

    finish the stylings

--- 12. MAJOR_PROJECT/models/listing.js
+++ 12. MAJOR_PROJECT/models/listing.js
@@ -14,9 +14,9 @@ const listingSchema = new Schema({
         },
         url : {
             type : String,
-            default :   "https://unsplash.com/photos/a-desk-with-a-computer-monitor-and-keyboard-XWPoEJnLQAc",
-            set : (v) => v === "" ? 
-        "https://unsplash.com/photos/a-desk-with-a-computer-monitor-and-keyboard-XWPoEJnLQAc"  : v },
+            default : "https://media.istockphoto.com/id/472899538/photo/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab.jpg?s=612x612&w=0&k=20&c=rz-WSe_6gKfkID6EL9yxCdN_UIMkXUBsr67884j-X9o=",
+            set : (v) => v=== "" ? 
+           "https://media.istockphoto.com/id/472899538/photo/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab.jpg?s=612x612&w=0&k=20&c=rz-WSe_6gKfkID6EL9yxCdN_UIMkXUBsr67884j-X9o=" : v },
          //img does not exist / undefined / null /  ( this is when there is no option for img , means for testing)
         
         //img is present but link is empty  ( this condition is for client )
