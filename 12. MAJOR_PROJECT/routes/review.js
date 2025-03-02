c63596f0406850fd4400a13212563200ef159b00
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-03-02 17:34:40 +0530

    completing the authorisation for reviews ,  applying mvc design pattern

--- 12. MAJOR_PROJECT/routes/review.js
+++ 12. MAJOR_PROJECT/routes/review.js
@@ -4,31 +4,14 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js")
 const Review = require("../models/review.js")              //review model
 const Listing =   require("../models/listing.js");           //listing model
-const {  validateReview } = require("../middleware.js")  
+const {  validateReview  , isLoggedIn, isReviewAuthor} = require("../middleware.js")  
 
+const reviewController = require("../controllers/reviews.js")
 
 //reviews - post route
-router.post("/" ,  validateReview, wrapAsync( async (req,res) => {    ///listings/:id/reviews
-    console.log(req.params.id)
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
- 
-    listing.reviews.push(newReview)
-    await newReview.save();
-    await listing.save(); // updating the listing
-    req.flash("success" , "New Review Created!")
-   res.redirect(`/listings/${listing._id}`)
- 
- }));
+router.post("/" , isLoggedIn,  validateReview, wrapAsync(reviewController.createReview));
  
  //Delete - Review Route
-router.delete( "/:reviewId" , wrapAsync ( async (req,res) => {   ///listings/:id/reviews/:reviewId
-     let { id , reviewId } = req.params;   
- 
-     await Listing.findByIdAndUpdate(id , { $pull : { reviews : reviewId } });
-     await Review.findByIdAndDelete(reviewId);
-     req.flash("success" , "Review Deleted!")
-     res.redirect(`/listings/${id}`)
- }));
+router.delete( "/:reviewId" ,isLoggedIn, isReviewAuthor, wrapAsync (reviewController.destroyReview));
 
  module.exports = router;
