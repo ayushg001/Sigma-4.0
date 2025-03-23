dad0fc849ba8b86927f4d656a4e2b6a12f31417c
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-03-23 19:22:55 +0530

    learning state in react

---
+++ 15. REACT/react-state-class/src/App.jsx
@@ -0,0 +1,19 @@
+import './Counter.jsx'
+import './App.css'
+import Counter from './Counter.jsx';
+import LikeButton from './LikeButton.jsx';
+
+
+function App() {
+  
+ return (
+   <>
+   <h1>States In React</h1>
+   <LikeButton/>
+   </>
+  );
+}
+
+
+
+export default App
